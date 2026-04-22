8feacc3710725c660bb0762acbd1e38855bf65ea
Ashwitha Abeera <35499793+Ashwithaa@users.noreply.github.com>
2026-04-22 06:58:42 +0530

    Calculate Intrest rate

--- example1.js
+++ example1.js
@@ -1,1 +1,7 @@
-console.log("Hello, World!");
+let p = 1000;
+let r = 5;
+let t = 2;
+
+let result = (p * r * t) / 100;
+//console.log("Simple Interest: " + result);
+document.getElementById("interest1").textContent = parseFloat(result);
